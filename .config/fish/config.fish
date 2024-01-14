2670ee91900739ec491f0823b6699e6f5aef8256
Dima Neiaglov <dima.neiaglov@gmail.com>
2024-01-15 00:35:44 +0400

    first commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,34 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+fish_add_path /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin /opt/X11/bin /opt/homebrew/opt/bin /Users/neiaglov/bin /Users/neiaglov/.cargo/bin /Users/neiaglov/sys/bin
+
+function fish_greeting
+echo "
+                        █████████    ███
+                       ███░░░░░███  ░░░
+  ██████   █████ ████ ░███    ░███  ████  ████████
+ ░░░░░███ ░░███ ░███  ░███████████ ░░███ ░░███░░███
+  ███████  ░███ ░███  ░███░░░░░███  ░███  ░███ ░░░
+ ███░░███  ░███ ░███  ░███    ░███  ░███  ░███
+░░████████ ░░███████  █████   █████ █████ █████
+ ░░░░░░░░   ░░░░░███ ░░░░░   ░░░░░ ░░░░░ ░░░░░
+            ███ ░███
+           ░░██████
+            ░░░░░░
+"
+end
+
+#
+# Prompt
+
+function fish_prompt
+echo (set_color yellow)$hostname(set_color normal) (prompt_pwd) "\$ "
+end
+
+#
+# Aliases
+function emacs
+    /Applications/Emacs.app/Contents/MacOS/Emacs $argv &
+end
+
+end
