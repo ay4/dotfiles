3e42358c3964d68f055ed22bd87b181f61222afb
Dima Neiaglov <dima.neiaglov@gmail.com>
2024-01-31 12:41:06 +0400

    Updated readme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+# Commands to run in interactive sessions can go here
+
 fish_add_path /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin /opt/X11/bin /opt/homebrew/opt/bin /Users/neiaglov/bin /Users/neiaglov/.cargo/bin /Users/neiaglov/sys/bin
+
+end
 
 function fish_greeting
 echo "
@@ -25,10 +28,3 @@ function fish_prompt
 echo (set_color yellow)$hostname(set_color normal) (prompt_pwd) "\$ "
 end
 
-#
-# Aliases
-function emacs
-    /Applications/Emacs.app/Contents/MacOS/Emacs $argv &
-end
-
-end
